94f045150a11af429d108ae27a892af6404d9da6
maroco <betterforwhat@gmail.com>
2019-07-22 10:27:28 +0900

    path add

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+PATH=/sbin:/usr/sbin:/usr/bin:/usr/local/bin
+
 echo 'The following "npm" command builds your Node.js/React application for'
 echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
